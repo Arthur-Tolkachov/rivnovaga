91806081fe394ef79ce4b8d7a5f4c507efb0ffa5
Arthur Tolkachov <babanjerelo@gmail.com>
2025-10-23 17:27:58 +0300

    [Feature] added section about

--- app/components/layout/Header/Header.tsx
+++ app/components/layout/Header/Header.tsx
@@ -1,16 +1,21 @@
 import Container from "@/app/components/ui/Container";
 
+import HeaderBg from "./HeaderBg";
 import HeaderBottom from "./HeaderBottom";
 import { HeaderTop } from "./HeaderTop";
 
 export function Header() {
   return (
-    <header className="fixed top-0 left-0 right-0 z-1">
-      <Container>
-        <HeaderTop />
+    <>
+      <header className="sticky top-[-70px] left-0 right-0 z-1 mb-[-182px]">
+        <HeaderBg />
 
-        <HeaderBottom />
-      </Container>
-    </header>
+        <Container className="relative z-1">
+          <HeaderTop />
+
+          <HeaderBottom />
+        </Container>
+      </header>
+    </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import About from "@/app/components/layout/About";
 import { Hero } from "@/app/components/layout/Hero";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+
+      <main>
+        <About />
+      </main>
     </div>
   );
 }
