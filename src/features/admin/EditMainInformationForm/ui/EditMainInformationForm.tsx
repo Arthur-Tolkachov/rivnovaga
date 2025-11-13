99055ceb8934819114827c909d0056800a8c384d
Arthur Tolkachov <babanjerelo@gmail.com>
2025-11-13 23:28:41 +0200

    [Fix] props

--- src/features/admin/EditMainInformationForm/ui/EditMainInformationForm.tsx
+++ src/features/admin/EditMainInformationForm/ui/EditMainInformationForm.tsx
@@ -8,14 +8,14 @@ import { MainInformationSchedule } from "./MainInformationSchedule";
 import { useEditMainInformationForm } from "../model/useEditMainInformationForm";
 
 export const EditMainInformationForm = () => {
-  const { methods, onSubmit } = useEditMainInformationForm();
+  const { methods, isFetching, onSubmit } = useEditMainInformationForm();
 
   return (
     <FormProvider {...methods}>
       <form onSubmit={onSubmit} className="flex flex-col gap-5">
-        <MainInformation />
-        <MainInformationAddress />
-        <MainInformationSchedule />
+        <MainInformation isFetching={isFetching} />
+        <MainInformationAddress isFetching={isFetching} />
+        <MainInformationSchedule isFetching={isFetching} />
       </form>
     </FormProvider>
   );
