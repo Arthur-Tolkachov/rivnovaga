9d9df3663178f11a78f2880db6504ad4ee35c2b0
Arthur Tolkachov <babanjerelo@gmail.com>
2025-12-08 20:34:18 +0200

    [Feature] added add block action

--- src/entities/usefulLinks/ui/UsefulLinksForm/UsefulLinksForm.tsx
+++ src/entities/usefulLinks/ui/UsefulLinksForm/UsefulLinksForm.tsx
@@ -1,3 +1,6 @@
+import { useFieldArray, useFormContext } from "react-hook-form";
+
+import { Button } from "@shared/ui/base/Button";
 import { FormActionButtons } from "@shared/ui/composite/FormActionButtons";
 
 import { UsefulLinksFields } from "./UsefulLinksFields";
@@ -9,19 +12,46 @@ export interface UsefulLinksFormProps {
   onSubmit: (event: React.BaseSyntheticEvent) => Promise<void>;
 }
 
+const EMPTY_BLOCK = {
+  title: "",
+  key: "",
+  data: [
+    {
+      key: "",
+      label: "",
+      link: "",
+    },
+  ],
+};
+
 export const UsefulLinksForm: React.FC<UsefulLinksFormProps> = ({
   isFetching,
   isLoading,
   onReset,
   onSubmit,
-}) => (
-  <form onSubmit={onSubmit} className="flex flex-col gap-8">
-    <UsefulLinksFields isFetching={isFetching} />
+}) => {
+  const { control } = useFormContext();
+  const { fields, append } = useFieldArray({ control, name: "useful_links" });
 
-    <FormActionButtons
-      isFetching={isFetching}
-      isLoading={isLoading}
-      onReset={onReset}
-    />
-  </form>
-);
+  const onAddBlock = () => {
+    append(EMPTY_BLOCK);
+  };
+
+  return (
+    <form onSubmit={onSubmit} className="flex flex-col gap-8">
+      <UsefulLinksFields isFetching={isFetching} fields={fields} />
+
+      {!isFetching && (
+        <Button color="secondary" size="sm" onClick={onAddBlock}>
+          Додати блок
+        </Button>
+      )}
+
+      <FormActionButtons
+        isFetching={isFetching}
+        isLoading={isLoading}
+        onReset={onReset}
+      />
+    </form>
+  );
+};
--- src/shared/ui/composite/FormActionButtons/FormActionButtons.tsx
+++ src/shared/ui/composite/FormActionButtons/FormActionButtons.tsx
@@ -13,17 +13,11 @@ export const FormActionButtons: React.FC<FormActionButtonsProps> = ({
   onReset,
 }) => {
   if (isFetching) {
-    return (
-      <Skeleton
-        className="w-full h-[64px]"
-        containerClassName="grid grid-cols-[151px_164px] gap-5"
-        count={2}
-      />
-    );
+    return <Skeleton className="w-full h-[104px]" count={1} />;
   }
 
   return (
-    <div className="flex gap-5">
+    <div className="flex gap-5 p-5 bg-secondary-light sticky bottom-5">
       <Button
         type="submit"
         variant="filled"
